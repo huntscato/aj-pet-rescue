cb8ab605984c4d00ff81763c42249ce354d4adfe
Hunter <hgouldthorpe95@gmail.com>
2025-02-06 21:24:57 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/**** LOGIN PAGE ****/
+
+const loginForm = document.querySelector('#login-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');  
+
+loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const name = nameInput.value;
+    const email = emailInput.value;
+
+    const loginData = { name, email };
+
+    try {
+        const response = await fetch('https://frontend-take-home-service.fetch.com/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(loginData),
+            credentials: 'include',
+        });
+
+        if (response.ok) {
+            console.log('Login Successful!');
+            window.location.href = '/search.html';
+        } else {
+            alert('Login failed. Please check your credentials.')
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+    }
+});
+
